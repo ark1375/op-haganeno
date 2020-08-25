5f1e1d0c22986f0c1ccad61849196daeccc899c4
ark1375 <ark.blackhat@gmail.com>
2020-08-25 20:12:38 +0430

    Sectioning loading image completed

--- sectioning/sectioning.py
+++ sectioning/sectioning.py
@@ -1,17 +1,37 @@
-#   Author ARK1375
-#   Date: Aug 24 2020
-#
-#   Summary:
-#
+"""
+    Author: ARK1375
+    Date:   24 Aug 2020
+    Mod:    25 Aug 2020     19:27
+    Description:
 
+"""
 import PIL as pil
 from PIL import Image
+import numpy as np
 
-loc = r"./data/single_data/"
 
-def load(name):
+
+#location of the files
+
+locs = {'loc_train' : r"./data/single_data/train/" , 'loc_test' : r"./data/single_data/test/"}
+
+"""
+A function for loading the image as a numpy ndarray
+"""
+def load(name, tr_ts):
+
+    loc = locs[tr_ts]
+    
     image = Image.open(loc+name)
-    return image.getdata()
+    data = np.asarray(image)
+    #check if the data is valid
+    if (data.shape == (28,28)):
+        return data
 
-print(  len(    list(load("id_5_label_4.png")   )  ) )
+def set_locs(trainloc, testloc):
+    locs['loc_train'] = trainloc
+    locs['loc_test'] = testloc
+
+print(load("id_5_label_4.png" , "loc_train"))
+
 
